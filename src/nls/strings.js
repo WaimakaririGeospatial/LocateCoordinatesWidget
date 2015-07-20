9dc5d52c44c13ec6d1ab2eed4102a34356756865
Shaun Weston <shaun_weston@eagle.co.nz>
2015-07-21 10:06:55 +1200

    Initial commit.

---
+++ src/nls/strings.js
@@ -0,0 +1,16 @@
+define({
+  root: ({
+      coordinateSystem: "Coordinate System",
+      locate: "Locate",
+      clear: "Clear",
+      symbology: "Symbology",
+      location: "Location",
+      closestAddress: "Closest address to this point",
+      noAddress: "No address found",
+      geographicXLabel: "Longitude (X):",
+      geographicYLabel: "Latitude (Y):",
+      projectedXLabel: "Easting (X):",
+      projectedYLabel: "Northing (Y):",
+      errorMessageCoordinates: "Invalid coordinates. Please try again."
+  })
+});
---
+++ src/setting/nls/strings.js
@@ -0,0 +1,10 @@
+define({
+  root: ({
+      geometryServiceURL: "Geometry Service URL",
+      addressLocatorServiceURL: "Address Locator Service URL",
+      coordinateSystem: "Coordinate System",
+      spatialReference: "Spatial Reference",
+      example: "Example",
+      ok: "OK"
+  })
+});
